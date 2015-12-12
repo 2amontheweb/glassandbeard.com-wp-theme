c32a1a6c1cb82379c983c0b9a8dcb9d7b55a8441
Jimmy Pocock <jimmypocock@yahoo.com>
2015-12-11 22:57:38 -0600

    Added fonts sass, js, and config files.

--- footer.php
+++ footer.php
@@ -11,18 +11,15 @@
 
 ?>
 
-	</div><!-- #content -->
+      <footer id="main-footer" class="site-footer" role="contentinfo">
+        <div class="container">
+          <h2>Footer</h2>
+        </div>
+      </footer><!-- #colophon -->
+      <?php wp_footer(); ?>
+    </div><!-- #page -->
 
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'glass-and-beard' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'glass-and-beard' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'glass-and-beard' ), 'glass-and-beard', '<a href="http://jimmypocock.com" rel="designer">Jimmy Pocock</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div><!-- #page -->
+    <script src="<?php bloginfo('template_directory') ?>/js/bundled.js"></script>
 
-<?php wp_footer(); ?>
-
-</body>
+  </body>
 </html>
---
+++ scripts/home.js
@@ -0,0 +1,32 @@
+/* globals $: false */
+/* jshint strict: false */
+
+$(function () {
+
+  // set #home-top to full screen
+  function fullscreen(){
+    $('#home-top, #home-top .overlay').css({
+      height: ($(window).height() - 50)
+    });
+  }
+
+  fullscreen();
+  $(window).resize(function() {
+    fullscreen();
+  });
+
+  // manage fixed nav on scroll
+  var navbar = $('.navbar');
+  var origOffsetY = navbar.offset().top;
+  function scroll() {
+    if ($(window).scrollTop() >= origOffsetY) {
+      $('.navbar').addClass('sticky');
+      $('.content').addClass('navbar-padding');
+    } else {
+      $('.navbar').removeClass('sticky');
+      $('.content').removeClass('navbar-padding');
+    }
+  }
+
+  document.onscroll = scroll;
+});
