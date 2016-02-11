944a51c65a9728bbd754d6ae1b17695fadbbeaa7
Jimmy Pocock <jimmypocock@yahoo.com>
2016-02-10 22:52:57 -0600

    Got the about and portfolio html in

--- template-parts/home-about.php
+++ template-parts/home-about.php
@@ -4,9 +4,64 @@
  *
  * @package Glass and Beard
  */
+  $about_photo = get_field('about_photo');
+
+  $welcome_title = get_field('welcome_title');
+  $welcome_message = get_field('welcome_message');
+
+  $about_header_one = get_field('about_header_one');
+  $about_content_one = get_field('about_content_one');
+
+  $about_header_two = get_field('about_header_two');
+  $about_content_two = get_field('about_content_two');
+
+  $about_header_three = get_field('about_header_three');
+  $about_content_three = get_field('about_content_three');
+
+  $about_header_four = get_field('about_header_four');
+  $about_content_four = get_field('about_content_four');
+
+  $signature_image = get_field('signature_image');
 ?>
 
 <div id="home-about" class="home-section">
   <a name="home-about-link" class="scroll-destination"></a>
-  <h2>About</h2>
+  <div class="container">
+    <div class="row">
+
+      <div class="col-sm-5 left">
+        <h2>About</h2>
+        <div class="about-photo" style="background-image:url(<?php echo $about_photo['url']; ?>)">
+          <div class="lines"></div>
+        </div>
+      </div>
+
+      <div class="col-sm-7">
+        <div class="col-sm-6 middle">
+          <h3 class="welcome-title"><?php echo $welcome_title; ?></h3>
+          <p class="welcome-messgae"><?php echo $welcome_message; ?></p>
+          <img src="<?php echo $signature_image['url']; ?>">
+        </div>
+        <div class="col-sm-6 right">
+          <div class="one">
+            <h4 class="about-header"><?php echo $about_header_one; ?></h4>
+            <p class="about-content"><?php echo $about_content_one; ?></p>
+          </div>
+          <div class="two">
+            <h4 class="about-header"><?php echo $about_header_two; ?></h4>
+            <p class="about-content"><?php echo $about_content_two; ?></p>
+          </div>
+          <div class="three">
+            <h4 class="about-header"><?php echo $about_header_three; ?></h4>
+            <p class="about-content"><?php echo $about_content_three; ?></p>
+          </div>
+          <div class="four">
+            <h4 class="about-header"><?php echo $about_header_four; ?></h4>
+            <p class="about-content"><?php echo $about_content_four; ?></p>
+          </div>
+        </div>
+      </div>
+
+    </div>
+  </div>
 </div>
--- template-parts/home-portfolio.php
+++ template-parts/home-portfolio.php
@@ -4,9 +4,74 @@
  *
  * @package Glass and Beard
  */
+
+  $portfolio_left_image = get_field('portfolio_left_image');
+
+  $pf_photography_image_one = get_field('pf_photography_image_one');
+  $pf_photography_image_two = get_field('pf_photography_image_two');
+  $pf_photography_image_three = get_field('pf_photography_image_three');
+  $pf_photography_image_four = get_field('pf_photography_image_four');
+
+  $pf_logo_image_one = get_field('pf_logo_image_one');
+  $pf_logo_image_two = get_field('pf_logo_image_two');
+  $pf_logo_image_three = get_field('pf_logo_image_three');
+  $pf_logo_image_four = get_field('pf_logo_image_four');
+
+  $pf_website_image_one = get_field('pf_website_image_one');
+  $pf_website_image_two = get_field('pf_website_image_two');
+  $pf_website_image_three = get_field('pf_website_image_three');
+  $pf_website_image_four = get_field('pf_website_image_four');
+
+  $pf_magazine_image_one = get_field('pf_magazine_image_one');
+  $pf_magazine_image_two = get_field('pf_magazine_image_two');
+  $pf_magazine_image_three = get_field('pf_magazine_image_three');
+  $pf_magazine_image_four = get_field('pf_magazine_image_four');
+
+  $pf_typography_image_one = get_field('pf_typography_image_one');
+  $pf_typography_image_two = get_field('pf_typography_image_two');
+  $pf_typography_image_three = get_field('pf_typography_image_three');
+  $pf_typography_image_four = get_field('pf_typography_image_four');
 ?>
 
 <div id="home-portfolio" class="home-section">
   <a name="home-portfolio-link" class="scroll-destination"></a>
-  <h2>Portfolio</h2>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-5 portfolio-main-image" style="background-image:url(<?php echo $portfolio_left_image['url']; ?>)">
+      </div>
+      <div class="col-sm-7">
+        <div class="portfolio">
+          <ul class="nav nav-tabs">
+            <li role="presentation" class="active"><a href="#pf_photography" data-toggle="tab">Photography</a></li>
+            <li role="presentation"><a href="#pf_logo" data-toggle="tab">Logo</a></li>
+            <li role="presentation"><a href="#pf_website" data-toggle="tab">Website</a></li>
+            <li role="presentation"><a href="#pf_magazine" data-toggle="tab">Magazine</a></li>
+            <li role="presentation"><a href="#pf_typography" data-toggle="tab">Typography</a></li>
+          </ul>
+          <div class="tab-content clearfix">
+
+            <div class="tab-pane active" id="pf_photography">
+              <h3>something somehting yada yada yada something somehting yada yada yada something somehting yada yada yada</h3>
+            </div>
+
+            <div class="tab-pane active" id="pf_logo">
+              <h3>Content's background color is the same for the tab</h3>
+            </div>
+
+            <div class="tab-pane" id="pf_website">
+              <h3>We use the class nav-pills instead of nav-tabs which automatically creates a background color for the tab</h3>
+            </div>
+
+            <div class="tab-pane" id="pf_magazine">
+              <h3>We applied clearfix to the tab-content to rid of the gap between the tab and the content</h3>
+            </div>
+
+            <div class="tab-pane" id="pf_typography">
+              <h3>We use css to change the background color of the content to be equal to the tab</h3>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
